28bda556f82fbf431b95cad2d62c427be5a21dca
NikitaMatlaiev <mem92762@gmail.com>
2024-09-09 22:33:45 +0300

    Add files via upload

---
+++ Основи програмування Матлаєв 11-ІСТ 1.py
@@ -0,0 +1,9 @@
+name = "Nikita"
+surname = "Matlaiev"
+age = 17
+HW = "Hello World"
+print (name, surname, age, HW)
+print ("name -", type(name))
+print ("surname -", type(surname))
+print ("age -", type(age))
+print ("HW -", type(HW))
